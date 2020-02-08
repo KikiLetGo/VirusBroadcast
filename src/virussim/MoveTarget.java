ceec2e398dec734a5ac15f494d2b2851be64b56e
dy_55 <minidy20001001@outlook.com>
2020-02-08 21:12:00 +0800

    Structure Changed

--- src/virussim/MoveTarget.java
+++ src/virussim/MoveTarget.java
@@ -6,31 +6,13 @@ package virussim;
  * @author: Bruce Young
  * @date: 2020年02月02日 17:47
  */
-public class MoveTarget {
-    private int x;
-    private int y;
-    private boolean arrived = false;
-
+public class MoveTarget extends Point {
+    
     public MoveTarget(int x, int y) {
-        this.x = x;
-        this.y = y;
+        super(x, y);
     }
 
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
+    private boolean arrived = false;
 
     public boolean isArrived() {
         return arrived;
