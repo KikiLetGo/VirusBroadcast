d18059792c384a4e6deae941df6aca01724b5ead
PlanetMoon <xxx@xxx.com>
2020-02-11 20:34:19 +0800

    Updated

--- src/Constants.java
+++ src/Constants.java
@@ -8,7 +8,7 @@
  */
 public class Constants {
 
-    public static int ORIGINAL_COUNT = 50;//初始感染数量
+    public static int ORIGINAL_COUNT = 25;//初始感染数量
     public static float BROAD_RATE = 0.8f;//传播率
     public static float SHADOW_TIME = 140;//潜伏时间，14天为140
     public static int HOSPITAL_RECEIVE_TIME = 10;//医院收治响应时间
